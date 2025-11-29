8cd299cfd7a2f5c1445321892a169f1d50cc0877
Ahmet Şafak Yıldırım <114856193+ahm3texe@users.noreply.github.com>
2025-11-29 17:26:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ import rvc.lib.zluda
 
 # Import Tabs
 from tabs.inference.inference import inference_tab
-#from tabs.train.train import train_tab
-#from tabs.extra.extra import extra_tab
-#from tabs.report.report import report_tab
-#from tabs.download.download import download_tab
-#from tabs.tts.tts import tts_tab
-#from tabs.voice_blender.voice_blender import voice_blender_tab
-#from tabs.plugins.plugins import plugins_tab
-#from tabs.settings.settings import settings_tab
-#from tabs.realtime.realtime import realtime_tab
+from tabs.train.train import train_tab
+from tabs.extra.extra import extra_tab
+from tabs.report.report import report_tab
+from tabs.download.download import download_tab
+from tabs.tts.tts import tts_tab
+from tabs.voice_blender.voice_blender import voice_blender_tab
+from tabs.plugins.plugins import plugins_tab
+from tabs.settings.settings import settings_tab
+from tabs.realtime.realtime import realtime_tab
 
 # Run prerequisites
 from core import run_prerequisites_script
@@ -82,32 +82,32 @@ with gr.Blocks(
     with gr.Tab(i18n("Inference")):
         inference_tab()
 
-    with gr.Tab(i18n("Training")):
-        train_tab()
+    #with gr.Tab(i18n("Training")):
+        #train_tab()
 
-    with gr.Tab(i18n("TTS")):
-        tts_tab()
+    #with gr.Tab(i18n("TTS")):
+        #tts_tab()
 
-    with gr.Tab(i18n("Voice Blender")):
-        voice_blender_tab()
+    #with gr.Tab(i18n("Voice Blender")):
+        #voice_blender_tab()
 
-    with gr.Tab(i18n("Realtime")):
-        realtime_tab()
+    #with gr.Tab(i18n("Realtime")):
+        #realtime_tab()
 
-    with gr.Tab(i18n("Plugins")):
-        plugins_tab()
+    #with gr.Tab(i18n("Plugins")):
+        #plugins_tab()
 
-    with gr.Tab(i18n("Download")):
-        download_tab()
+    #with gr.Tab(i18n("Download")):
+        #download_tab()
 
-    with gr.Tab(i18n("Report a Bug")):
-        report_tab()
+    #with gr.Tab(i18n("Report a Bug")):
+        #report_tab()
 
-    with gr.Tab(i18n("Extra")):
-        extra_tab()
+    #with gr.Tab(i18n("Extra")):
+        #extra_tab()
 
-    with gr.Tab(i18n("Settings")):
-        settings_tab()
+    #with gr.Tab(i18n("Settings")):
+        #settings_tab()
 
     gr.Markdown(
         """
